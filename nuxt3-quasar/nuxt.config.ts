e781b71c35ca47616a1842f6a3d1d4d50c80b206
Minu Song <smw0807@gmail.com>
2023-06-10 13:00:20 +0900

    퀘이사 레이아웃 샘플 적용 분리 작업 예정

--- nuxt3-quasar/nuxt.config.ts
+++ nuxt3-quasar/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+/**
+ * 퀘이사 공식홈페이지
+ * https://quasar.dev/
+ *
+ * 레이아웃 예제 모음
+ * https://quasar.dev/layout/gallery/
+ */
 export default defineNuxtConfig({
   plugins: [],
   modules: ['nuxt-quasar-ui'],
