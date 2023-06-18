b832d93b5eb8b25b20574d905c23ae20a16d68d8
Minu Song <smw0807@gmail.com>
2023-06-18 19:46:47 +0900

    firestore database 사용할 페이지 뷰 파일 추가

---
+++ nuxt3-quasar/pages/firebase/database.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+<template>
+  <q-card dark bordered class="bg-brown-7 my-card">
+    <q-card-section>
+      <div class="text-h6">Firebase / Firestore Database</div>
+      <div class="text-subtitle2">firestore</div>
+    </q-card-section>
+
+    <q-separator dark inset />
+
+    <q-card-section>
+      파이어베이스에서 제공하는 firestore database를 nuxt-vuefire 모듈을 이용해
+      만든 게시판
+    </q-card-section>
+  </q-card>
+</template>
