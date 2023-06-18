fa0e01bdee639b4684e8959e65512ceb38236fe3
Minu Song <smw0807@gmail.com>
2023-06-18 22:34:33 +0900

    nuxt.config.ts에 vuefire 모듈 적용

--- nuxt3-quasar/nuxt.config.ts
+++ nuxt3-quasar/nuxt.config.ts
@@ -1,11 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const {
+  firebase_apiKey,
+  firebase_authDomain,
+  firebase_projectId,
+  firebase_databaseURL,
+  firebase_storageBucket,
+  firebase_messagingSenderId,
+  firebase_appId,
+  firebase_measurementId,
+} = process.env;
 export default defineNuxtConfig({
   plugins: [],
-  modules: ['nuxt-quasar-ui'],
+  modules: ['nuxt-quasar-ui', 'nuxt-vuefire'],
   quasar: {
     lang: 'ko-KR',
     extras: {
       fontIcons: ['material-icons'],
+    },
+  },
+  vuefire: {
+    config: {
+      apiKey: firebase_apiKey,
+      authDomain: firebase_authDomain,
+      projectId: firebase_projectId,
+      storageBucket: firebase_storageBucket,
+      messagingSenderId: firebase_messagingSenderId,
+      appId: firebase_appId,
+      measurementId: firebase_measurementId,
     },
   },
 });
