57743c5d164b5feeb2e8fcce102c3e7fb5b3ba2f
Minu Song <smw0807@gmail.com>
2023-06-23 23:15:05 +0900

    nuxt-vuefire 삭제로 옵션들 제거

--- nuxt3-quasar/nuxt.config.ts
+++ nuxt3-quasar/nuxt.config.ts
@@ -1,33 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const {
-  firebase_apiKey,
-  firebase_authDomain,
-  firebase_projectId,
-  firebase_databaseURL,
-  firebase_storageBucket,
-  firebase_messagingSenderId,
-  firebase_appId,
-  firebase_measurementId,
-} = process.env;
 export default defineNuxtConfig({
   plugins: [],
-  modules: ['nuxt-quasar-ui', 'nuxt-vuefire'],
+  modules: ['nuxt-quasar-ui'],
   quasar: {
     lang: 'ko-KR',
     extras: {
       fontIcons: ['material-icons'],
-    },
-  },
-  vuefire: {
-    config: {
-      apiKey: firebase_apiKey,
-      authDomain: firebase_authDomain,
-      databaseURL: firebase_databaseURL,
-      projectId: firebase_projectId,
-      storageBucket: firebase_storageBucket,
-      messagingSenderId: firebase_messagingSenderId,
-      appId: firebase_appId,
-      measurementId: firebase_measurementId,
     },
   },
 });
