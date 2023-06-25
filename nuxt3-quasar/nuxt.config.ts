831d26b964536f2922251c03ccf63993c4ea0dac
Minu Song <smw0807@gmail.com>
2023-06-25 17:14:21 +0900

    파이어베이스 설정값 runtimeConfig에 할당

--- nuxt3-quasar/nuxt.config.ts
+++ nuxt3-quasar/nuxt.config.ts
@@ -1,11 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const {
+  firebase_apiKey,
+  firebase_authDomain,
+  firebase_projectId,
+  firebase_databaseURL,
+  firebase_storageBucket,
+  firebase_messagingSenderId,
+  firebase_appId,
+  firebase_measurementId,
+} = process.env;
 export default defineNuxtConfig({
-  plugins: [],
+  plugins: [{ src: '~/plugins/firebase-init', mode: 'client' }],
   modules: ['nuxt-quasar-ui'],
   quasar: {
     lang: 'ko-KR',
     extras: {
       fontIcons: ['material-icons'],
+    },
+  },
+  runtimeConfig: {
+    public: {
+      apiKey: firebase_apiKey,
+      authDomain: firebase_authDomain,
+      databaseURL: firebase_databaseURL,
+      projectId: firebase_projectId,
+      storageBucket: firebase_storageBucket,
+      messagingSenderId: firebase_messagingSenderId,
+      appId: firebase_appId,
+      measurementId: firebase_measurementId,
     },
   },
 });
