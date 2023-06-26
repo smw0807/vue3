2967195395ad3a1674b4e4a43b798aba1dc7da0c
Minu Song <smw0807@gmail.com>
2023-06-26 23:10:47 +0900

    파이어베이스 초기화 플러그인

--- nuxt3-quasar/plugins/firebase-init.ts
+++ nuxt3-quasar/plugins/firebase-init.ts
@@ -1,36 +1,15 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import type { FirebaseOptions, FirebaseApp } from 'firebase/app';
-import type { Firestore } from 'firebase/firestore';
 
 export default defineNuxtPlugin(nuxtApp => {
   const config = useRuntimeConfig();
-  const {
-    firebase_apiKey,
-    firebase_authDomain,
-    firebase_projectId,
-    firebase_databaseURL,
-    firebase_storageBucket,
-    firebase_messagingSenderId,
-    firebase_appId,
-    firebase_measurementId,
-  } = config.public;
-
-  const firebaseConfig: FirebaseOptions = {
-    apiKey: firebase_apiKey,
-    authDomain: firebase_authDomain,
-    projectId: firebase_projectId,
-    databaseURL: firebase_databaseURL,
-    storageBucket: firebase_storageBucket,
-    messagingSenderId: firebase_messagingSenderId,
-    appId: firebase_appId,
-    measurementId: firebase_measurementId,
-  };
-
-  //파이어베이스 초기화
-  const app: FirebaseApp = initializeApp(firebaseConfig);
-  //파이어스토어
-  const fireStore: Firestore = getFirestore(app);
-
-  nuxtApp.provide('firestore', fireStore);
+  const firebaseApp = initializeApp({
+    apiKey: config.public.FB_API_KEY,
+    appId: config.public.FB_APP_ID,
+    authDomain: config.public.FB_AUTH_DOMAIN,
+    databaseURL: config.public.FB_DATABASE_URL,
+    measurementId: config.public.FB_MEASUREMENT_ID,
+    messagingSenderId: config.public.FB_MESSAGING_SENDER_ID,
+    projectId: config.public.FB_PROJECT_ID,
+    storageBucket: config.public.FB_STORAGE_BUCKET,
+  });
 });
