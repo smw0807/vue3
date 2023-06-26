0127abc1fc79f09432789ee8da147ae273c79d8f
Minu Song <smw0807@gmail.com>
2023-06-26 21:42:21 +0900

    env 키값 대문자 스네이크 케이스로 변경

--- nuxt3-quasar/nuxt.config.ts
+++ nuxt3-quasar/nuxt.config.ts
@@ -1,14 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const {
-  firebase_apiKey,
-  firebase_authDomain,
-  firebase_databaseURL,
-  firebase_projectId,
-  firebase_storageBucket,
-  firebase_messagingSenderId,
-  firebase_appId,
-  firebase_measurementId,
-} = process.env;
 export default defineNuxtConfig({
   plugins: [{ src: '~/plugins/firebase-init', mode: 'client' }],
   modules: ['nuxt-quasar-ui'],
@@ -20,14 +10,14 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      FB_API_KEY: firebase_apiKey,
-      FB_AUTH_DOMAIN: firebase_authDomain,
-      FB_DATABASE_URL: firebase_databaseURL,
-      FB_PROJECT_ID: firebase_projectId,
-      FB_STORAGE_BUCKET: firebase_storageBucket,
-      FB_MESSAGING_SENDER_ID: firebase_messagingSenderId,
-      FB_APP_ID: firebase_appId,
-      FB_MEASUREMENT_ID: firebase_measurementId,
+      FB_API_KEY: process.env.FIREBASE_API_KEY,
+      FB_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FB_DATABASE_URL: process.env.FIREBASE_DATABASE_RUL,
+      FB_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FB_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FB_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FB_APP_ID: process.env.FIREBASE_APP_ID,
+      FB_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
     },
   },
 });
