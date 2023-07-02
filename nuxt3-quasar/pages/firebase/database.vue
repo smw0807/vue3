ff293b132bf7edc27bb2977149626e003c22a8ca
Minu Song <smw0807@gmail.com>
2023-07-02 16:39:20 +0900

    파이어베이스 데이터 표시 퀘이사 테이블 컴포넌트에 가져온 데이터 표시. ref를 사용해서 데이터가 proxy라 format 함수말고 표시할 수 있는 방법 찾아야함

--- nuxt3-quasar/pages/firebase/database.vue
+++ nuxt3-quasar/pages/firebase/database.vue
@@ -1,15 +1,58 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, unref, computed } from 'vue';
 import type { Ref } from 'vue';
 import type { DocumentData } from 'firebase/firestore';
+import type { QTableColumn } from 'quasar';
 
 let lists: Ref<{ id: string; data: DocumentData }[] | []> = ref([]);
 // let lists: { id: string; data: DocumentData }[] | [] = []; //반응형이 아니라 화면에 안그려짐
+// let rows = computed(() => unref(lists.value));
 
 onMounted(async () => {
   lists.value = await getFirestoreData('test-board');
   // lists = await getFirestoreData('test-board'); //반응형이 아니라 화면에 안그려짐
 });
+
+const columns: QTableColumn[] = [
+  {
+    name: 'title',
+    label: '제목',
+    field: 'title',
+    align: 'left',
+    headerStyle: 'width: 20%',
+    format(val, row) {
+      return row.data.title;
+    },
+  },
+  {
+    name: 'wireter',
+    label: '작성자',
+    field: 'data.writer',
+    align: 'center',
+    format(val, row) {
+      return row.data.writer;
+    },
+  },
+  {
+    name: 'createdAt',
+    label: '작성일',
+    field: 'data.createdAt',
+    align: 'center',
+    format(val, row) {
+      const date = row.data.createdAt.toDate();
+      return date.toLocaleString();
+    },
+  },
+  {
+    name: 'viewer',
+    label: '조회수',
+    field: 'data.viewer',
+    align: 'center',
+    format(val, row) {
+      return row.data.viewer;
+    },
+  },
+];
 </script>
 <template>
   <q-card dark bordered class="bg-brown-7 my-card">
@@ -28,9 +71,14 @@ onMounted(async () => {
 
   <q-separator class="q-mt-md q-mb-xs" />
 
-  <ul>
-    <li v-for="list in lists" :key="list.id">
-      <span>{{ list }}</span>
-    </li>
-  </ul>
+  <div class="q-pa-md">
+    <q-table
+      bordered
+      title=""
+      :rows="lists"
+      :columns="columns"
+      no-data-label="No Data."
+      row-key="name"
+    />
+  </div>
 </template>
