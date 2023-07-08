0153b8bcb1e2ef1f7a460935509dd070ec7033ca
Minu Song <smw0807@gmail.com>
2023-07-08 18:46:51 +0900

    eslintrc, prettierrc 추가

---
+++ nuxt3-quasar/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+};
