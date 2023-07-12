f910cd7e543c0716dd13e28c1e2e4a3d947a988c
Minu Song <smw0807@gmail.com>
2023-07-13 00:40:42 +0900

    Refactor: 현재 로그인한 사용자 정보 함수 기능 변경 async/await 방식에서 콜백 방식으로 변경함 권장 방식이라함...

--- nuxt3-quasar/composables/useAuth.ts
+++ nuxt3-quasar/composables/useAuth.ts
@@ -52,40 +52,32 @@ export const useGetUserAuth = (): User | null => {
 };
 
 /**
- * 현재 로그인한 사용자 가져오기.
+ * 현재 로그인한 사용자 정보
+ * <pre>
  * 현재 사용자를 가져올 때 권장하는 방법(공식문서).
  * Auth 객체에 관찰자 설정.
  * https://firebase.google.com/docs/auth/web/manage-users?hl=ko&authuser=0
+ *
+ * Firbase 인증 상태는 비동기적으로 변경되기 때문에 콜백 패턴을 사용한다.
+ * 구독패턴(subscription pattern)의 한 종류로, 이벤트가 발생하면 콜백함수가 실행되는 방식이다.
+ * </pre>
  * @returns User | null
  */
-const getNowUserAuth = (): Promise<User | null> => {
-  return new Promise((resolve, reject) => {
-    try {
-      onAuthStateChanged(getFirebaseAuth(), (user) => {
-        if (user) {
-          resolve(user);
-        } else {
-          resolve(null);
-        }
-      });
-    } catch (err) {
-      reject(err);
-    }
-  });
-};
-/**
- * 현재 로그인한 사용자 정보
- * @returns User | null
- */
-export const useGetNowUserAuth = async (): Promise<User | null> => {
-  let result = null;
+export const useGetNowUserAuth = (
+  callback: (user: User | null) => void,
+): void => {
   try {
-    result = await getNowUserAuth();
+    onAuthStateChanged(getFirebaseAuth(), (user) => {
+      if (user) {
+        callback(user);
+      } else {
+        callback(null);
+      }
+    });
   } catch (err) {
     console.error(err);
     throw new Error('현재 사용자 정보 가져오기 실패');
   }
-  return result;
 };
 
 /**
