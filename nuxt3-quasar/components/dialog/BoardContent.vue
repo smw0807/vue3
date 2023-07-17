49138e66d35bdf68d9393ecef49e134372c3525f
Minu Song <smw0807@gmail.com>
2023-07-17 21:50:07 +0900

    Feat: 글 쓰기 컴포넌트 validate 기능 추가 제목, 내용에 대한 정합성 체크 기능 추가 통과 시에만 confirm이 활성화 하도록 로직 추가

--- nuxt3-quasar/components/dialog/BoardContent.vue
+++ nuxt3-quasar/components/dialog/BoardContent.vue
@@ -11,13 +11,6 @@ interface IFrom {
   title: string;
   content: string;
 }
-
-// 입력폼
-const formData: IFrom = reactive({
-  title: '',
-  content: '',
-});
-
 /**
  * submit : 입력된 formData를 위로 전달함 @return IForm
  * close : 닫기 버튼 클릭시 위로 전달함 @return void
@@ -33,6 +26,28 @@ const props = withDefaults(defineProps<Props>(), {
 // 컴포넌트 오픈 여부
 const cOpen = computed(() => props.isOpen);
 
+// form ref
+const boardContentForm = ref();
+
+// 입력폼
+const formData: IFrom = reactive({
+  title: '',
+  content: '',
+});
+
+// title validate
+const titleRules = (v: string): boolean | string => {
+  if (!v) return '제목을 입력해주세요.';
+  else if (v.length > 20) return '제목은 최대 20자까지 입력 가능합니다.';
+  return true;
+};
+// contnet validate
+const contentRules = (v: string): boolean | string => {
+  if (!v) return '내용을 입력해주세요.';
+  else if (v.length > 500) return '내용은 최대 500자까지 입력 가능합니다.';
+  return true;
+};
+
 // confirm 오픈 여부
 const confirmOpen: Ref<boolean> = ref(false);
 // confirm 제목
@@ -42,6 +57,16 @@ const confirmText =
   props.mode === 'ins'
     ? '입력한 내용을 등록하시겠습니까?'
     : '입력한 내용으로 글을 수정하시겠습니까?';
+// confirm
+const confirm = async () => {
+  try {
+    // form 정합성 체크
+    const valid = await boardContentForm.value.validate();
+    if (valid) confirmOpen.value = true;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 // 입력한 값들을 위로 전파
 const submit = () => {
@@ -59,29 +84,33 @@ const close = (): void => {
         <div class="text-h6">글쓰기</div>
       </q-card-section>
 
-      <q-card-section class="q-pt-none">
-        <q-input
-          v-model="formData.title"
-          autofocus
-          filled
-          label="제목"
-          color="purple-12"
-          @keyup.enter="submit"
-        />
-      </q-card-section>
+      <q-form ref="boardContentForm">
+        <q-card-section class="q-pt-none">
+          <q-input
+            v-model="formData.title"
+            :rules="[titleRules]"
+            autofocus
+            filled
+            label="제목"
+            color="purple-12"
+            @keyup.enter="submit"
+          />
+        </q-card-section>
 
-      <q-card-section class="q-pt-sm">
-        <q-input
-          v-model="formData.content"
-          type="textarea"
-          filled
-          label="내용"
-          input-style="height:200px;"
-        />
-      </q-card-section>
+        <q-card-section class="q-pt-sm">
+          <q-input
+            v-model="formData.content"
+            :rules="[contentRules]"
+            type="textarea"
+            filled
+            label="내용"
+            input-style="height:200px;"
+          />
+        </q-card-section>
+      </q-form>
 
       <q-card-actions align="right" class="text-primary">
-        <q-btn @click="confirmOpen = true" color="primary" label="등록" />
+        <q-btn @click="confirm" color="primary" label="등록" />
         <q-btn @click="close" outline label="닫기" />
       </q-card-actions>
     </q-card>
