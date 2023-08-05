d7957f7dac85e686b27640a33445ed3e20ad4d70
Minu Song <smw0807@gmail.com>
2023-08-05 15:15:35 +0900

    Feat: 커스텀 타입 추가

---
+++ nuxt3-quasar/types/index.d.ts
@@ -0,0 +1,15 @@
+type NullType = null;
+type FirebaseBoardDocType = {
+  content: string;
+  createdAt: {
+    seconds: number;
+    nanoseconds: number;
+  };
+  id: string;
+  title: string;
+  viewer: string;
+  writer: string;
+  writerID: string;
+};
+type RowType = FirebaseBoardDocType | NullType;
+type BoardInsertType = Pick<FirebaseBoardDocType, 'title' | 'content'>;
