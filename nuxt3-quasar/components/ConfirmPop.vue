552ceff37cb58256d939201655e46350a79f8905
Minu Song <smw0807@gmail.com>
2023-09-02 18:42:44 +0900

    feat: confirm 용 컴포넌트 추가 글 등록, 수정하는 컴포넌트 안에 confirm 로직을 만들어 쓰다가 삭제 기능을 구현하려고하니 문제가 좀 발생함... 별도의 컴포넌트로 처리하는게 깔끔할 것 같아서 추가함

---
+++ nuxt3-quasar/components/ConfirmPop.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+interface Props {
+  isOpen: boolean;
+  title: string;
+  text: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  isOpen: false,
+  title: '',
+  text: '',
+});
+
+const emits = defineEmits(['submit']);
+
+const cOpen = computed(() => props.isOpen);
+
+const submit = (v: boolean): void => {
+  emits('submit', v);
+};
+</script>
+<template>
+  <q-dialog
+    v-model="cOpen"
+    persistent
+    transition-show="scale"
+    transition-hide="scale"
+    style="z-index: 99999"
+  >
+    <q-card class="bg-teal text-white" style="width: 300px">
+      <q-card-section>
+        <div class="text-h6">{{ title }}</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ text }}
+      </q-card-section>
+
+      <q-card-actions align="right" class="bg-white text-teal">
+        <q-btn flat label="저장" @click="submit(true)" v-close-popup />
+        <q-btn flat label="취소" @click="submit(false)" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
