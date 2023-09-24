9f76a235109ea4123ed26efe1e08debfb28e0908
Minu Song <smw0807@gmail.com>
2023-09-24 14:51:57 +0900

    commnet : 글로벌 타입정의 파일 주석 추가

--- nuxt3-quasar/types/index.d.ts
+++ nuxt3-quasar/types/index.d.ts
@@ -1,3 +1,4 @@
+// 메뉴 타입
 type MenuType = {
   icon?: string; // 메뉴 아이콘
   text: string; //  메뉴 이름
@@ -5,6 +6,8 @@ type MenuType = {
 };
 
 type NullType = null;
+
+// 파이어베이스 게시판 컬렉션 데이터 타입
 type FirebaseBoardDocType = {
   content: string;
   createdAt: {
@@ -17,6 +20,10 @@ type FirebaseBoardDocType = {
   writer: string;
   writerID: string;
 };
+
+// 파이어베이스 게시판 컬렉션 1개 데이터 타입
 type RowType = FirebaseBoardDocType | NullType;
+// 파이어베이스 게시판 컬렉션 데이터 추가할 때 타입
 type BoardInsertType = Pick<FirebaseBoardDocType, 'title' | 'content'>;
+// 파이어베이스 게시판 컬렉션 데이터 수정할 때 타입
 type BoardUpdateType = Pick<FirebaseBoardDocType, 'id' | 'title' | 'content'>;
