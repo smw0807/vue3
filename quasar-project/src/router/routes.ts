5fa3274dea8f6ba6b2501b2a12078818de336320
Minu Song <smw0807@gmail.com>
2023-12-12 01:41:06 +0900

    init: infinite scoll

--- quasar-project/src/router/routes.ts
+++ quasar-project/src/router/routes.ts
@@ -6,6 +6,16 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
+  {
+    path: '/infinite-scroll',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('pages/infinite-scroll/InfiniteScroll.vue'),
+      },
+    ],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
