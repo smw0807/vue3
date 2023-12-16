d884ab02a62f2d2819eb6f4f9f24fe33c0655661
Minu Song <smw0807@gmail.com>
2023-12-17 02:06:53 +0900

    라우터 수정

--- quasar-project/src/router/routes.ts
+++ quasar-project/src/router/routes.ts
@@ -4,15 +4,16 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-  },
-  {
-    path: '/infinite-scroll',
-    component: () => import('layouts/MainLayout.vue'),
     children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
       {
-        path: '',
+        path: '/infinite-scroll',
         component: () => import('pages/infinite-scroll/InfiniteScroll.vue'),
+      },
+      {
+        path: '/array-test',
+        component: () => import('pages/ArrayTest.vue'),
+        name: 'ArrayTest',
       },
     ],
   },
