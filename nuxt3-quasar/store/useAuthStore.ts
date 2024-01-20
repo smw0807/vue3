125017986fca2ba2a7c025687639000aa8615fba
Minu Song <smw0807@gmail.com>
2024-01-20 17:16:56 +0900

    refactor: pinia optionsAPI -> compositionAPI

--- nuxt3-quasar/store/useAuthStore.ts
+++ nuxt3-quasar/store/useAuthStore.ts
@@ -1,15 +1,20 @@
+import { computed, ref } from 'vue';
 import { defineStore } from 'pinia';
+import type { Ref } from 'vue';
 import type { User } from 'firebase/auth';
 
-export const useAuthStore = defineStore('user', {
-  state: () => ({
-    userAuth: null as User | null,
-  }),
-  getters: {
-    //  유저 정보 가져오기
-    getUserAuth(): User | null {
-      return this.userAuth;
-    },
-  },
-  actions: {},
+export const useAuthStore = defineStore('user', () => {
+  const userAuth: Ref<User | null> = ref(null);
+  const state = { userAuth };
+
+  const getUserAuth = computed(() => userAuth.value);
+  const getters = { getUserAuth };
+
+  const actions = {};
+
+  return {
+    ...state,
+    ...getters,
+    ...actions,
+  };
 });
