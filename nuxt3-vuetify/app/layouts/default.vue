e980a124931aab8e9f5726bfd7c6332f740049e3
smw0807 <smw0807@gmail.com>
2025-09-01 21:33:58 +0900

    뷰티파이 레이아웃 적용

--- nuxt3-vuetify/app/layouts/default.vue
+++ nuxt3-vuetify/app/layouts/default.vue
@@ -1,5 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const links = [
+  ['mdi-inbox-arrow-down', 'Inbox'],
+  ['mdi-send', 'Send'],
+  ['mdi-delete', 'Trash'],
+  ['mdi-alert-octagon', 'Spam'],
+];
+
+const drawer = ref(true);
+</script>
 <template>
-  <div>
+  <v-app id="inspire">
+    <v-app-bar class="bg-blue-darken-2 border-b">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer">
+      <v-sheet class="pa-4" color="grey-lighten-4">
+        <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
+
+        <div>john@google.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          :prepend-icon="icon"
+          :title="text"
+          link
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <slot />
-  </div>
+  </v-app>
 </template>
