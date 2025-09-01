3c1e39fed5de195beacfcd2c25783202afe1dc50
smw0807 <smw0807@gmail.com>
2025-09-01 22:24:55 +0900

    태그 수정

--- nuxt3-vuetify/app/layouts/default.vue
+++ nuxt3-vuetify/app/layouts/default.vue
@@ -13,19 +13,19 @@ const drawer = ref(true);
 <template>
   <v-app id="inspire">
     <v-app-bar class="bg-blue-darken-2 border-b">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
 
       <v-app-bar-title>Application</v-app-bar-title>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer">
       <v-sheet class="pa-4" color="grey-lighten-4">
-        <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
+        <v-avatar class="mb-4" color="grey-darken-1" size="64" />
 
         <div>john@google.com</div>
       </v-sheet>
 
-      <v-divider></v-divider>
+      <v-divider />
 
       <v-list>
         <v-list-item
@@ -34,7 +34,7 @@ const drawer = ref(true);
           :prepend-icon="icon"
           :title="text"
           link
-        ></v-list-item>
+        />
       </v-list>
     </v-navigation-drawer>
 
